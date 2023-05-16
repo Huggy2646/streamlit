a46f4819bb75dbecd3f2d5502974ef14a161bee9
Hugi <81164267+Huggy2646@users.noreply.github.com>
2023-05-16 12:53:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[30,30]+[37.76,-122.4],columns=['lat','lon'])
 st.map(df)
 
-rnad = np.random.normal(1,2,size=20)
+rand = np.random.normal(1,2,size=20)
 fig,ax=plt.subplots()
 ax.hist(rand,bins=15)
 st.pyplot(fig)
